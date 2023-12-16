adcb757081e9d7c0ab52e1bf58180c0cbf148bfc
tchandu1012 <117568994+tchandu1012@users.noreply.github.com>
2023-12-16 18:30:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,18 +5,52 @@ import requests
 from core.config import settings
 
 app = FastAPI(title=settings.PROJECT_NAME,version=settings.PROJECT_VERSION)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
 
 MIRO_ACCESS_TOKEN = os.getenv("MIRO_ACCESS_TOKEN")
-MIRO_API_BASE_URL = "https://api.miro.com/v2/boards/"
+MIRO_API_BASE_URL = "https://api.miro.com/v2"
 OPENAI_key = os.getenv("OpenAI_Key")
 
+
+headers = {
+    'Authorization': f'Bearer {MIRO_ACCESS_TOKEN}',
+    'Content-Type': 'application/json'
+}
+
+def remove_html_tags(text):
+    clean = re.compile('<.*?>')
+    return re.sub(clean, '', text)
+    
 @app.get("/")
 def hello_api():
     return {"msg":"Hello, This is IBC MIRO Test API Service 🚀"}
 
+def get_item_info(item_id):
+    response = requests.get(f'{MIRO_API_BASE_URL}/boards/{BOARD_ID}/items/{item_id}', headers=headers)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print(f"Failed to get frame info. Reason: {response.content.decode('utf-8')}.")
+        return None
+
+def get_frame_info(frame_id):
+    response = requests.get(f'{MIRO_API_BASE_URL}/boards/{BOARD_ID}/frames/{frame_id}', headers=headers)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        error_reason = response.json().get("message") if response.json().get("message") else "Failed to fetch Miro Frame"
+        raise HTTPException(status_code=response.status_code, detail=error_reason)
+
+        
 def get_miro_frames(board_id):
-    url = f"{MIRO_API_BASE_URL}{board_id}/frames"
-    response = requests.get(url, headers={"Authorization": f"Bearer {MIRO_ACCESS_TOKEN}"})
+    url = f"{MIRO_API_BASE_URL}/boards/{board_id}/frames"
+    response = requests.get(url, headers)
 
     if response.status_code == 200:
         return response.json()
@@ -25,8 +59,8 @@ def get_miro_frames(board_id):
         raise HTTPException(status_code=response.status_code, detail=error_reason)
 
 def get_miro_cards(board_id, frame_id):
-    url = f"{MIRO_API_BASE_URL}{board_id}/frames/{frame_id}/cards"
-    response = requests.get(url, headers={"Authorization": f"Bearer {MIRO_ACCESS_TOKEN}"})
+    url = f"{MIRO_API_BASE_URL}/boards/{board_id}/frames/{frame_id}/cards"
+    response = requests.get(url, headers)
 
     if response.status_code == 200:
         return response.json()
