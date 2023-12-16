a931c97350f168a2d398f291a068f3776f683e07
tchandu1012 <117568994+tchandu1012@users.noreply.github.com>
2023-12-16 22:46:28 +0530

    Update genFeaturesFromEpic.py

--- genFeaturesFromEpic.py
+++ genFeaturesFromEpic.py
@@ -42,7 +42,8 @@ def generate_features(epic_description):
     "frequency_penalty": 0,
     "presence_penalty": 0
   })
-    #print(f"IN Calling OpenAI ${headers}")
+
+  print(f"IN Calling OpenAI for generate_features ")
 
   response = requests.request("POST", url, headers=headers, data=payload)
   print(response.text)
