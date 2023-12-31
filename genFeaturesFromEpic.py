a5ce3fa14458505decddf4ae86f2701b56e2f0b7
ChandraT <117568994+tchandu1012@users.noreply.github.com>
2023-12-31 12:36:06 +0530

    Update genFeaturesFromEpic.py

--- genFeaturesFromEpic.py
+++ genFeaturesFromEpic.py
@@ -1,9 +1,16 @@
 import requests
 import json
 import os
+from core.config import logger
+from dotenv import load_dotenv
+
+# Load the .env file
+load_dotenv()
+logger.info("In OpenAI......")
 
 OPENAI_URL = "https://api.openai.com/v1/chat/completions"
 OPENAI_key = os.getenv("OpenAI_Key")
+logger.info (f"OpenAIKey is {OPENAI_key}")
 
 headers = {
     'Authorization': f'Bearer {OPENAI_key}',
@@ -11,9 +18,9 @@ headers = {
 }
 
 
-def generate_features(epic_description):
+def generate_features(epic_description, top_n=5):
   """
-  Generates top five features based on the given epic description.
+  Generates top 'n' features based on the given epic description.
 
   Parameters:
   - epic_description (str): The description of the epic.
@@ -68,13 +75,13 @@ def generate_features(epic_description):
       },
       {
         "role": "system",
-        "content": """Generate top five Features which will satisfy the given Epic description strictly in a Syntactical correct JSONArray format.
+        "content": f"""Generate top '{top_n}'  Features which will satisfy the given Epic description strictly in a Syntactical correct JSONArray format.
         Review all generated features thoroughly and provide detailed hypothesis, acceptance criteria, leading indicators, non-functional requirements (NFRs),
         and objectives and key results (OKRs). Estimate 'Business Value', 'Time Criticality', and 'RR/OE' for each of the features in Fibonacci numbers,
         with at least one feature having a value '1' as Business Value. Provide detailed explaination reasoning for each of the estimations
         so that everyone can understand and agree. Generate output strictly in a Syntactical correct JSONArray format.
-        ## Sample JSONArray Ouput  as follows :
-        {  "ArraySize": 2, 'Features': [{'feature': { 'Title': 'Sample Title', 'Hypothesis': 'Sample ', 'Acceptance Criteria': '[Given] Sample [When] sample [Then] sample', 'Leading Indicators': 'Sample ', 'Non-functional Requirements': 'Sample', 'Business Value': { 'value': 8, 'rationale': 'Sample' }, 'Time Criticality': { 'value': 8, 'rationale': 'Sample.' }, 'RR/OE': { 'value': 3, 'rationale': 'Sample.' } } }, // Include similar structures for the remaining features...] }' """
+        ## Sample JSONArray Ouput  as follows :"""
+        """{  "ArraySize": 2, 'Features': [{'feature': { 'Title': 'Sample Title', 'Hypothesis': 'Sample ', 'Acceptance Criteria': '[Given] Sample [When] sample [Then] sample', 'Leading Indicators': 'Sample ', 'Non-functional Requirements': 'Sample', 'Business Value': { 'value': 8, 'rationale': 'Sample' }, 'Time Criticality': { 'value': 8, 'rationale': 'Sample.' }, 'RR/OE': { 'value': 3, 'rationale': 'Sample.' } } }, // Include similar structures for the remaining features...] }' """
       }
     ],
     "temperature": 0,
@@ -84,7 +91,7 @@ def generate_features(epic_description):
     "presence_penalty": 0
   })
 
-  print(f"In Calling OpenAI for generate_features ")
+  print("In Calling OpenAI for generate_features ")
 
   response = requests.request("POST", url, headers=headers, data=payload)
   print(response.text)
@@ -95,5 +102,3 @@ def generate_features(epic_description):
   print(choices_0)
   return choices_0
 
-# card_data ="Create a Mentor-Mentee Initiative"
-# generate_features(card_data)
