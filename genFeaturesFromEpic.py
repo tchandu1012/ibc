3bbaac307b3bf092dec52a7b3adc4956e1fd8339
tchandu1012 <117568994+tchandu1012@users.noreply.github.com>
2023-12-31 13:11:39 +0530

    commit changes to openai to convert print to logging

--- genFeaturesFromEpic.py
+++ genFeaturesFromEpic.py
@@ -91,10 +91,10 @@ def generate_features(epic_description, top_n=5):
     "presence_penalty": 0
   })
 
-  print("In Calling OpenAI for generate_features ")
+  logger.info(f"In Calling OpenAI for generate_features for a Epic Description : {epic_description}")
 
   response = requests.request("POST", url, headers=headers, data=payload)
-  print(response.text)
+  logger.info(f"OpenAI response : {response.text}")
   # Parse the response text to a Python dictionary
   response_data = json.loads(response.text)
   # Access choices[0] from the response data
